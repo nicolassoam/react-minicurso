c569a6e99495e6ac4c37ad75977156c3a5fc3a20
nicolassoam <nsm4456@gmail.com>
2022-10-18 15:17:01 -0300

    hello world

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Helloworld = () =>{
+    return <div>Hello World</div>
+}
+
+ReactDOM.render(
+    <Helloworld/>,
+    document.getElementById('root')
+)
